e68ab40069dc64168375bc3a954dce018caf9c7a
001101010 <abhilash18082002@gmail.com>
2024-08-08 17:00:19 +0530

    uploadToCloudinary

--- src/Utils/UploadToCloudinary.js
+++ src/Utils/UploadToCloudinary.js
@@ -1,21 +1,25 @@
-export const uploadToCloudinary = async (pics,fileType) => {
-    if (pics) {
-      
-      const data = new FormData();
-      data.append("file", pics);
-      data.append("upload_preset", "instagram");
-      data.append("cloud_name", "dnbw04gbs");
-  
-      const res = await fetch(`https://api.cloudinary.com/v1_1/dnbw04gbs/${fileType}/upload`, {
-        method: "post",
-        body: data,
-      })
-        
-        const fileData=await res.json();
-        console.log("url : ", fileData.url.toString());
-        return fileData.url.toString();
-  
-    } else {
-      console.log("error");
-    }
-  };
+const cloud_name = "doe7yoj5o";
+const upload_preset = "Abhi-Social";
+
+export const uploadToCloudinary = async (pics, fileType) => {
+  if (pics && fileType) {
+    const data = new FormData();
+    data.append("file", pics);
+    data.append("upload_preset", upload_preset);
+    data.append("cloud_name", cloud_name);
+
+    const res = await fetch(
+      `https://api.cloudinary.com/v1_1/${cloud_name}/${fileType}/upload`,
+
+      { method: "post", body: data }
+    );
+
+    console.log("res",res);
+
+    const fileData = await res.json();
+    return fileData.url;
+   
+  } else {
+    console.log("error------");
+  }
+};
