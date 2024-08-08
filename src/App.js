096f9e90ad308ced1a30d7c641803cedafce57f0
001101010 <abhilash18082002@gmail.com>
2024-08-08 16:58:17 +0530

    responsive

--- src/App.js
+++ src/App.js
@@ -1,33 +1,54 @@
-
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Authentication from './pages/Authentication/Authentication';
-import HomePage from './pages/HomePage/HomePage';
-import Message from './pages/Message/Message';
+import Authentication from './Components/Authentication/Authentication';
+import HomePage from './Components/HomePage';
 import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
-import { getProfileAction } from './Redux/Auth/auth.action';
+import { useEffect, useState } from 'react';
+import { getUserProfile } from './Store/Auth/Action';
 
+import darkTheme from './Theme/DarkTheme';
+import lightTheme from './Theme/LightTheme';
+import { Box, Button, CssBaseline } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Palette } from '@mui/icons-material';
+import VerifiedSuccess from './Components/VerifiedSuccess/VerifiedSuccess';
 
 function App() {
-
   const dispatch=useDispatch();
-  const jwt = localStorage.getItem("jwt");
-  const {auth} = useSelector(store=>store);
-  useEffect(()=>{
-    dispatch(getProfileAction(jwt))
-  },[jwt, dispatch])
-  
-  return (
-    <div className="">
-     <Routes>
-  <Route path="/*" element={auth.user ? <HomePage /> : <Authentication />} />
-  <Route path="/message" element={<Message />} />
-  <Route path="/*" element={<Authentication />} />
-</Routes>
+  const {auth}=useSelector(store=>store);
+  const jwt = localStorage.getItem("jwt")
+  const [currentTheme,setCurrentTheme]=useState("");
+  const {theme}=useSelector(store=>store);
 
-     
-    </div>
+  useEffect(()=>{
+
+    if(jwt){
+      dispatch(getUserProfile(jwt))
+    }
+  
+  },[auth.jwt,jwt])
+
+  useEffect(()=>{
+setCurrentTheme(localStorage.getItem("theme"))
+  },[theme.currentTheme])
+ 
+  console.log("them ",theme.currentTheme)
+  return (
+    <ThemeProvider theme={currentTheme==="dark"? darkTheme:lightTheme} className="">
+      <CssBaseline />
+      <Box sx={{}}>
+        {/* <Button variant='content' color='success'>Check Theme</Button> */}
+          <Routes>
+        <Route path='/*' element={ auth.user?.fullName? <HomePage/>:<Authentication/>}></Route>
+        <Route path='/signin' element={<Authentication/>}></Route>
+        <Route path='/signup' element={<Authentication/>}></Route>
+        <Route path='/verified' element={<VerifiedSuccess/>}></Route>
+        {/* <Route path='/profile' element={<HomePage/>}></Route> */}
+      </Routes>
+      </Box>
+    
+      
+    </ThemeProvider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,23 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import { store } from './Redux/store';
+import { store } from './Store/store';
 import { BrowserRouter } from 'react-router-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const clientId="961098686120-p4pulrerjjelk6bhsbegd84m4r4mbdk7.apps.googleusercontent.com"
 root.render(
   <React.StrictMode>
-    <Provider store={store} >
+    <Provider store={store}>
       <BrowserRouter>
-      <App />
+      <GoogleOAuthProvider clientId={clientId}> <App /> </GoogleOAuthProvider>;
+       
       </BrowserRouter>
-    
     </Provider>
-    
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
