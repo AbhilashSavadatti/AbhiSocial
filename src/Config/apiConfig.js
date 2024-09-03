54861f38eef20ea6310afc9841cfcecbce511206
Abhilash S <abhilash18082002@gmail.com>
2024-09-03 18:23:03 +0530

    updated api url

--- src/Config/apiConfig.js
+++ src/Config/apiConfig.js
@@ -1,14 +1,15 @@
 import axios from "axios";
-const jwtToken = localStorage.getItem("jwt")
-export const API_BASE_URL = 'twitter-server-production-aa83.up.railway.app';
+
+const jwtToken = localStorage.getItem("jwt");
+
+export const API_BASE_URL = 'https://twitter-server-production-aa83.up.railway.app';
 
 export const api = axios.create({
   baseURL: API_BASE_URL, 
   headers: {
-    'Authorization':`Bearer ${jwtToken}`,
+    'Authorization': `Bearer ${jwtToken}`,
     'Content-Type': 'application/json',
   },
 });
 
-// Assuming you have the JWT token stored in a variable called jwtToken
-
+// Now you can use `api` to make requests to your server
